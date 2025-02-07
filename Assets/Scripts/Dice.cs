5a962b9007d1bbe835ad1642773a8548ebe44da1
Hapotuni116 <168194969+Hapotuni116@users.noreply.github.com>
2025-02-07 07:56:52 -0600

    Dice

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,20 +1,74 @@
 using UnityEngine;
+using System.Collections;
 
 public class Dice : MonoBehaviour
 {
-    [SerializeField] private GameObject dice;
-    [SerializeField] private GameObject table;
+    [Header("Die Forces")]
+    [SerializeField] private float torqueMinimum = 0.1f;
+    [SerializeField] private float torqueMaximum = 21f;
+    [SerializeField] private float throwStrength = 10f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Transform _trans;
+    private Rigidbody _rb;
+    private int  _num;
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    private void OnEnable()
     {
-        dice.SetActive(false);
-        table.SetActive(false);
+        _trans = transform;
+        _rb = GetComponent<Rigidbody>();
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void RollDice(){
+        _rb.AddForce(Vector3.up *  throwStrength, ForceMode.Impulse);
+
+        _rb.AddTorque(_trans.forward * Random.Range(torqueMinimum, torqueMaximum) + _trans.up * Random.Range(torqueMinimum, torqueMaximum)+ _trans.right * Random.Range(torqueMinimum, torqueMaximum));
+        StartCoroutine(WaitForStop());
     }
+
+    private IEnumerator WaitForStop(){
+
+        yield return new WaitForFixedUpdate();
+        while(_rb.angularVelocity.sqrMagnitude > 0.1){
+            yield return new WaitForFixedUpdate();
+        }
+        CheckDice();
+    }
+
+    private void CheckDice(){
+        float frontBack = Mathf.Round(Vector3.Dot(_trans.up.normalized, Vector3.up));
+        float upDown = Mathf.Round(Vector3.Dot(_trans.forward.normalized, Vector3.up));
+        float LeftRight = Mathf.Round(Vector3.Dot(_trans.right.normalized, Vector3.up));
+
+        switch(upDown){
+            case 1:
+                Debug.Log("1");
+                break;
+            case -1:
+                Debug.Log("6");
+                break;
+        }
+
+        switch(LeftRight){
+            case 1:
+                Debug.Log("4");
+                break;
+            case -1:
+                Debug.Log("3");
+                break;
+        }
+
+        switch(frontBack){
+            case 1:
+                Debug.Log("5");
+                break;
+            case -1:
+                Debug.Log("2");
+                break;
+        }
+    }
+
 }
